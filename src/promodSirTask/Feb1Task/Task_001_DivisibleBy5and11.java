613bc14937885d8cfb4807d1c6642596438e99de
sushma <sushmreddy.g526@gmail.com>
2025-02-03 10:58:16 +0530

    this is BonusCalculation

---
+++ src/promodSirTask/Feb1Task/Task_001_DivisibleBy5and11.java
@@ -0,0 +1,20 @@
+package promodSirTask;
+
+import java.util.Scanner;
+
+public class Task_001_DivisibleBy5and11 {
+    public static void main(String[] args) {
+       
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter number");
+            int num = sc.nextInt();
+
+            if (num % 5 == 0 && num % 11 == 0) {
+                System.out.println("the " + num + " is divisible by both 5 and 11");
+            } else {
+                System.out.println("the " + num + " is not divisible by both 5 and 11");
+            }
+
+        }
+    }
+
