966414b7e99a7bf5e7663b3e022b871ee22183f3
sushma <sushmreddy.g526@gmail.com>
2025-02-03 10:58:37 +0530

    this is amstrong

--- src/promodSirTask/Feb1Task/Task_001_DivisibleBy5and11.java
+++ src/promodSirTask/Feb1Task/Task_001_DivisibleBy5and11.java
@@ -1,10 +1,10 @@
-package promodSirTask;
+package promodSirTask.Feb1Task;
 
 import java.util.Scanner;
 
 public class Task_001_DivisibleBy5and11 {
     public static void main(String[] args) {
-       
+
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter number");
             int num = sc.nextInt();
